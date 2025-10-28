02e95e569c337ca851de809d8fb799f062c58e83
Lucas Gillis <lumgillis@gmail.com>
2025-10-27 21:21:23 -0300

    Setup With Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,13 @@ import './index.css'
 
 import Homepage from './routes/Homepage'
 import Login from './routes/Login'
-import AuthProvider from './components/appComponents/AuthProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
-        <AuthProvider>
-          <Route path='*' element={<Homepage />} />
-        </AuthProvider>
+        <Route path='*' element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
